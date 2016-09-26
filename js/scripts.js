211b070847b48f59038f2b0b46443378a51f3816
westleyc30 <westleyc30@gmail.com>
2016-09-25 19:30:33 -0700

    loading

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,16 @@
 var lat = '';
 var lon = '';
 var weatherAPI = '';
+var isCelcius;
+var weather = {};
 
 // SELECTOR VARIABLES
 var elLocation = document.querySelector('#location');
 var elWeather = document.querySelector('#weather');
 var elTemperature = document.querySelector('#temperature');
+var elTempButton = document.querySelector('#tempButton');
+
+elTempButton.onclick = degreeSwap;
 
 
 // LATITUDE AND LONGITUDE
@@ -15,7 +20,6 @@ if (navigator.geolocation) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
     weatherAPI = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat.toString() + '&lon=' + lon.toString() + '&appid=9fe1126f3544e9ea311e7312dca99844';
-    elLocation.innerHTML ='latitude: ' + lat + ' ' + 'longitude: ' + lon;
     sendRequest(weatherAPI);
   });
 }
@@ -27,9 +31,8 @@ function sendRequest(url) {
     if (request.readyState === 4 && request.status === 200) {
       // SUCCESSFUL loading
       var data = JSON.parse(request.responseText);
-      var weather = {};
-      weather.tempC = Math.round(data.main.temp - 273.15) + '&deg';
-      weather.tempF = Math.round(weather.tempC * (9 / 5) + 32) + '&deg';
+      weather.tempC = toC(data.main.temp);
+      weather.tempF = toF(data.main.temp);
       weather.desc = data.weather[0].main;
       weather.city = data.name;
       update(weather);
@@ -38,7 +41,26 @@ function sendRequest(url) {
   request.send();
 }
 
+function toC(k) {
+  return Math.round(k - 273.15) + '&deg' + 'C';
+}
+
+function toF(k) {
+  return Math.round(k * 9/5 - 459.67) + '&deg' + 'F';
+}
+
+function degreeSwap() {
+  if (isCelcius) {
+    isCelcius = false;
+    elTemperature.innerHTML = weather.tempF;
+  } else {
+    isCelcius = true;
+    elTemperature.innerHTML = weather.tempC;
+  }
+}
+
 function update(weather) {
+  isCelcius = false;
   elTemperature.innerHTML = weather.tempF;
   elLocation.innerHTML = weather.city;
   elWeather.innerHTML = weather.desc;
